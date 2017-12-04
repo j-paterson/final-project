4f476789c9cee3cd9f7a8d77ad2099a9dc713858
Jesse Paterson <jesse.r.pat@gmail.com>
2017-12-03 23:52:49 -0800

    added more functionality

--- contracts/Ticketsale.sol
+++ contracts/Ticketsale.sol
@@ -14,22 +14,31 @@ contract Ticketsale {
 	uint public saleOpens;
 	uint public showDate;
 	uint public refundWindow;
-	uint public remainingTickets;
+	uint public totalTickets;
+	uint public soldTickets;
 	uint public ticketCost;
 	uint public maxTickets;
+	uint private profits;
 	address[] buyers;
 
 	struct Buyer {
 	   uint ticketNumber;
 	}
 
-	mapping(address => Buyer) buyerInfo;
+	mapping(address => Buyer) private buyerInfo;
+	mapping(address => bool) private isGreeter;
+	mapping(address => bool) private hasEntered;
 
 	event TicketSold(address receiver, bool status);
 	event EtherRefunded(address receiver, bool status);
 
 	modifier isCreator() {
 		require(msg.sender == creator);
+		_;
+	}
+
+	modifier isGreeter() {
+		require(isGreeter[msg.sender] == true);
 		_;
 	}
 
@@ -43,23 +52,44 @@ contract Ticketsale {
 		_;
 	}
 
+	function admit(){
+
+	}
+
 	function Ticketsale(uint256 _totalSupply, uint256 _ticketCost, uint256 _maxTickets, uint256 _showDate) public{
 		saleOpens = now;
 		showDate = SafeMath.add(saleOpens, SafeMath.mul(_showDate, 1 minutes));
 		creator = msg.sender;
-		remainingTickets = _totalSupply;
+		totalTickets = _totalSupply;
 		ticketCost = _ticketCost;
 		maxTickets = _maxTickets;
 		showDate = _showDate;
+		soldTickets = 0;
+		isGreeter[msg.sender] = true;
 	}
 
-	/*
-	function purchaseTicket() saleHasNotEnded() public returns (bool)  {
-		return true;
-	}
-	*/
 
-	function refund() public returns (bool) {
+	function purchaseTicket() payable saleHasNotEnded() returns (bool) {
+		if((soldTickets < totalTickets) && ( >= ticketCost)){
+			buyers.push(msg.sender);
+			newBuyer Buyer = Buyer(soldTickets);
+			buyerInfo[msg.sender]=newBuyer;
+			profits+=msg.value
+			soldTickets+=1;
+			return true;
+		}
+		return false;
+	}
+
+	function addGreeter(address _greeter) isCreator() {
+		isGreeter[_greeter] = true;
+	}
+
+	function admit(address _atGate) isGreeter(){
+		hasEntered[_atGate] = true;
+	}
+
+	function refund() saleHasNotEnded() public returns (bool) {
 		bool good = true;
 		if (good) {
 			good = msg.sender.send(ticketCost);
@@ -68,9 +98,15 @@ contract Ticketsale {
 		return good;
 	}
 
+	function withdrawFunds() isCreator() public returns (uint) {
+		return profits;
+	}
+
 	function withdrawFunds() saleHasEnded() isCreator() public returns (bool) {
 		return creator.send(currentBalence);
 		selfdestruct(creator);
 	}
+
+	function() payable {}
 
 }
